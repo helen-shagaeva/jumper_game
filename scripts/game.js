bf2ea72f277f44cd5fe4b35a12d2879591a541b5
Vladimir <vmalikov92@gmail.com>
2012-09-26 19:00:19 +0300

    Added base functional

--- scripts/game.js
+++ scripts/game.js
@@ -1,23 +1,23 @@
 // Setup copter element and jumper model
-var jumper = new newJumper();
-buzzer = document.getElementById("jumper_id");
+var jumperObj = new newJumper();
+jumper = document.getElementById("jumper_id");
 
 var slat = new newSlat();
 slat_h = document.getElementById("slat_id");
 
 // Copy the "logical" object's position to the
 // element in the DOM
-draw = function(copter, slat) {
-	buzzer.style.left = copter.xpos;
-	buzzer.style.top = copter.ypos;
+draw = function(jumperObj, slat) {
+	jumper.style.left = jumperObj.xpos;
+	jumper.style.top = jumperObj.ypos;
 
     slat_h.style.top = slat.ypos;
     slat_h.style.left = slat.xpos;
 };
 
 update = function() {
-	jumper.updateJumper(jumper, slat);
-	draw(jumper, slat);
+	jumperObj.updateJumper(jumperObj, slat);
+	draw(jumperObj, slat);
 };
 
 setInterval(update, 50);	// 50msec = 20 frames per second
