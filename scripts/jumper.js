a8a5199f1b63f8d3775d6c44336ea9aa37b80f91
Alexander <saninosan@gmail.com>
2012-10-25 00:17:14 +0300

    Change jumper.html and scripts/jumper.js to 4a21a4d commit state

--- scripts/jumper.js
+++ scripts/jumper.js
@@ -4,12 +4,14 @@
  * Time: 5:44 PM
  */
 function Jumper() {
-    // Значение гравитации, влияющей на ускорение по ОУ
+    
+	this.width = 40;
+	this.height = 70;
+
+	this.image  = new Image();
+    this.image.src = 'images/human.png';
 	
-	//Очки за прыжки
-	this.point = 0;
-	
-	
+	// Значение гравитации, влияющей на ускорение по ОУ
     this.i_gravity = 2;
 
     // Трение объекта с воздухом,
@@ -23,7 +25,7 @@ function Jumper() {
     this.i_yShift = 6;
 
     // Координаты объекта (х, у)
-    this.i_xpos = 300;
+    this.i_xpos = 320;
     this.i_ypos = 300;
 
     // Ускорение по ОХ (изначально 0 - объект не движется по горизонтали)
@@ -37,10 +39,16 @@ function Jumper() {
 
     // Переменная, определяющая взлет и падение объекта
     this.b_boosting = false;
-
-    this.updateJumper = function (jumperObj, a_slats) {
-
-        winSizes = getWinSize();
+	
+	/*
+    this.drawJumper = function (canvas, i_xpos = this.i_xpos, i_ypos = this.i_ypos) {
+		var o_ctx = canvas.getContext('2d');
+		o_ctx.clearRect(0, 0, winSizes.myWidth, winSizes.myHeight);
+		o_ctx.drawImage(this.image, i_xpos, i_ypos);
+	};
+	*/
+	
+	this.updateJumper = function (jumperObj, a_slats) {
 
         // Трение - уменьшает ускорение объекта до 0.
         if (this.i_xacc > this.i_friction) {
@@ -60,11 +68,8 @@ function Jumper() {
         // Если объект пересекает ступень - задаем начальное положение, и ускорение объекту,
         // Иначе наращиваем ускорени объекту
         if(this.falling(a_slats)) {
-			var zz = jumperObj.point;
-            jumperObj.i_ypos = this.o_last_slat.ypos - a_slats[oneSlat].height_of_slat - 30;
+            jumperObj.i_ypos = this.o_last_slat.ypos - a_slats[oneSlat].height - 30;
             jumperObj.b_boosting = true;
-			jumperObj.point = zz+10;
-			sPoint(jumperObj.point);
             jumperObj.i_yacc = -30;
             jumperObj.i_xacc = 0;
         } else {
@@ -73,54 +78,130 @@ function Jumper() {
         }
 
 
-        document.onmousedown = function(e) {
-            //var direction=mouseShowHandler(e).x<winSizes.myWidth/2 ? 'leftside' : 'rightside';
-            if (mouseShowHandler(e).x < winSizes.myWidth / 2) {
-                // Проверяем не превысил ли обект скорость по ОХ
-                if(jumperObj.i_xacc - jumperObj.i_yShift < -jumperObj.i_yMaxacceleration) {
-                    jumperObj.i_xacc = -jumperObj.i_yMaxacceleration;
-                } else {
-                    jumperObj.i_xacc -= jumperObj.i_yShift;
-                }
-                if (jumperObj.i_xpos <= -20) jumperObj.i_xpos = winSizes.myWidth - 40;
-            }
-            else if (mouseShowHandler(e).x > winSizes.myWidth / 2) {
-                // Проверяем не превысил ли обект скорость по ОХ
-                if(jumperObj.i_xacc + jumperObj.i_yShift > jumperObj.i_yMaxacceleration) {
-                    jumperObj.i_xacc = jumperObj.i_yMaxacceleration;
-                } else {
-                    jumperObj.i_xacc += jumperObj.i_yShift;
-                }
-                if (jumperObj.i_xpos >= winSizes.myWidth - 60) jumperObj.i_xpos = -40;
-            }
-        }
-
-        document.onkeydown = function(e) {
+		/*-----------------------------------------------------------*/
+        /*-------------mouse and keyboard events:------------------- */
+		
+        document.onmousedown = function(event) {
             b_keyPressed = true;
-            if (b_keyPressed && getKeyCode(e) == "37") {
-                //alert('left');
-                // Проверяем не превысил ли обект скорость по ОХ
+			
+            if (mouseShowHandler(event).x < winSizes.myWidth / 2 && b_keyPressed) {
+                
+				// Проверяем не превысил ли обект скорость по ОХ
                 if(jumperObj.i_xacc - jumperObj.i_yShift < -jumperObj.i_yMaxacceleration) {
                     jumperObj.i_xacc = -jumperObj.i_yMaxacceleration;
                 } else {
                     jumperObj.i_xacc -= jumperObj.i_yShift;
                 }
-                if (jumperObj.i_xpos <= -20) jumperObj.i_xpos = winSizes.myWidth - 40;
-            }
-            else if (b_keyPressed && getKeyCode(e) == "39") {
-                //alert('right');
-                // Проверяем не превысил ли обект скорость по ОХ
+                if (jumperObj.i_xpos <= -20) {
+					jumperObj.i_xpos = winSizes.myWidth - 40;
+				}
+            
+			}
+            else if (mouseShowHandler(event).x > winSizes.myWidth / 2 && b_keyPressed) {
+                
+				// Проверяем не превысил ли обект скорость по ОХ
                 if(jumperObj.i_xacc + jumperObj.i_yShift > jumperObj.i_yMaxacceleration) {
                     jumperObj.i_xacc = jumperObj.i_yMaxacceleration;
                 } else {
                     jumperObj.i_xacc += jumperObj.i_yShift;
                 }
-                if (jumperObj.i_xpos >= winSizes.myWidth - 60) jumperObj.i_xpos = -40;
+                
+				if (jumperObj.i_xpos >= winSizes.myWidth - 60) {
+					jumperObj.i_xpos = -40;
+				}
             }
         }
-        document.onkeyup = function() {
-            b_keyPressed = false;
+		        
+		
+		document.onmouseup = function(event) {
+	    	b_keyPressed = false;
         }
+		
+		//==================
+		//==================
+		//==================
+		
+		document.onkeypress = function(event) {
+        	if (b_keyPressed){
+
+				switch (parseInt(getKeyCode(event))) {
+		            case 37:	// left
+						
+		                // Проверяем не превысил ли обект скорость по ОХ
+		                if(jumperObj.i_xacc - jumperObj.i_yShift < -jumperObj.i_yMaxacceleration) {
+		                    jumperObj.i_xacc = -jumperObj.i_yMaxacceleration;
+		                } else {
+		                    jumperObj.i_xacc -= jumperObj.i_yShift;
+		                }
+		                if (jumperObj.i_xpos <= -20) {
+							jumperObj.i_xpos = winSizes.myWidth - 40;
+						}
+			            break;
+		            case 39:	// right
+						
+	                	// Проверяем не превысил ли обект скорость по ОХ
+		                if(jumperObj.i_xacc + jumperObj.i_yShift > jumperObj.i_yMaxacceleration) {
+		                    jumperObj.i_xacc = jumperObj.i_yMaxacceleration;
+		                } else {
+		                    jumperObj.i_xacc += jumperObj.i_yShift;
+		                }
+		                if (jumperObj.i_xpos >= winSizes.myWidth - 60) {
+							jumperObj.i_xpos = -40;
+						}
+		                break;
+		        }
+				
+			}
+        }
+		
+		document.onkeydown = function(event) {
+        	switch (parseInt(getKeyCode(event))) {
+	            case 37:	// left
+	                b_keyPressed = true;
+					
+	                // Проверяем не превысил ли обект скорость по ОХ
+	                if(jumperObj.i_xacc - jumperObj.i_yShift < -jumperObj.i_yMaxacceleration) {
+	                    jumperObj.i_xacc = -jumperObj.i_yMaxacceleration;
+	                } else {
+	                    jumperObj.i_xacc -= jumperObj.i_yShift;
+	                }
+	                if (jumperObj.i_xpos <= -20) {
+						jumperObj.i_xpos = winSizes.myWidth - 40;
+					}
+		            break;
+	            case 39:	// right
+	                b_keyPressed = true;
+					
+                	// Проверяем не превысил ли обект скорость по ОХ
+	                if(jumperObj.i_xacc + jumperObj.i_yShift > jumperObj.i_yMaxacceleration) {
+	                    jumperObj.i_xacc = jumperObj.i_yMaxacceleration;
+	                } else {
+	                    jumperObj.i_xacc += jumperObj.i_yShift;
+	                }
+	                if (jumperObj.i_xpos >= winSizes.myWidth - 60) {
+						jumperObj.i_xpos = -40;
+					}
+	                break;
+	        }
+        }
+		
+		document.onkeyup = function(event) {
+			switch (parseInt(getKeyCode(event))) {
+	            case 37:	// left
+	                b_keyPressed = false;
+	                break;
+	            case 39:	// right
+	                b_keyPressed = false;
+	                break;
+	        }
+        }
+		/*-----------------------------------------------------------*/
+		
+		//exit to 'start menu'
+		if(this.explode()) {
+			alert('you loose ...');
+			document.location.href="start_menu.html";
+		}
     };
 
     // определяет пересекается ли объект со ступенью на следующем шаге.
@@ -131,10 +212,12 @@ function Jumper() {
     this.falling = function (a_slats) {
         for (oneSlat in a_slats) {
             if (!this.b_boosting) {
-                if (this.i_xpos >= a_slats[oneSlat].xpos && this.i_xpos <= a_slats[oneSlat].xpos + a_slats[oneSlat].width
-                        && this.i_ypos <= a_slats[oneSlat].ypos - a_slats[oneSlat].height_of_slat
-                        && this.i_ypos + this.i_yacc >= a_slats[oneSlat].ypos - a_slats[oneSlat].height_of_slat) {
-
+			
+			  if (this.i_xpos+this.width > a_slats[oneSlat].xpos && this.i_xpos < a_slats[oneSlat].xpos + a_slats[oneSlat].width
+                        && this.i_ypos <= a_slats[oneSlat].ypos - a_slats[oneSlat].height
+                        && this.i_ypos + this.i_yacc + this.height >= a_slats[oneSlat].ypos - a_slats[oneSlat].height) {
+					
+					//alert((this.i_ypos + this.i_yacc + this.height) +' - '+ (a_slats[0].ypos - a_slats[0].height));	
                     this.o_last_slat = a_slats[oneSlat];
                     return true;
                 }
@@ -142,4 +225,12 @@ function Jumper() {
         }
         return false;
     };
+	
+	//if person die
+	this.explode = function (){
+		if(this.i_ypos + this.height <= winSizes.myHeight){
+			return false;
+		}
+		return true;
+	}
 }
