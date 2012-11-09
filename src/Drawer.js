11b2f7b38e813c79d612cf7c8e1ac651d9b74390
Alexander <alsevdokimov@gmail.com>
2012-11-09 22:54:15 +0200

    Draw background on canvas

--- src/Drawer.js
+++ src/Drawer.js
@@ -3,6 +3,16 @@ function Drawer(canvas) {
 
     this.drawBackground = function (backgroundImg) {
         var context = this.canvas.getContext('2d');
-        context.drawImage(backgroundImg, 0, 0, backgroundImg.width, backgroundImg.height);
+        for (var i = 0; i * backgroundImg.height < this.canvas.height; i++) {
+
+            for (var j = 0; j * backgroundImg.width < this.canvas.width; j++) {
+                context.drawImage(backgroundImg, j * backgroundImg.width, i * backgroundImg.height,
+                    backgroundImg.width, backgroundImg.height);
+
+                //context.drawImage(backgroundImg, 0, 0, backgroundImg.width, backgroundImg.height);
+
+            }
+        }
+
     };
 }
--- src/game.js
+++ src/game.js
@@ -1,20 +1,23 @@
 var i_screenHeight = getWinSize().myHeight;
 var i_screenWidth = getWinSize().myWidth;
 
+var slats = new Array();
+
 var canvas = document.getElementById('canvas');
 
-canvas.offsetTop = (i_screenHeight - canvas.height) / 2;
-canvas.offsetLeft = (i_screenWidth - canvas.width) /2;
+//canvas.offsetTop = (i_screenHeight - canvas.height) / 2;
+//canvas.offsetLeft = (i_screenWidth - canvas.width) /2;
+
 var drawer = new Drawer(canvas);
+
+update = function() {
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    drawer.drawBackground(backgroundImg);
+};
 
 var backgroundImg = new Image();
 backgroundImg.src = "../img/background.png";
 backgroundImg.onload = function() {
-    drawer.drawBackground(backgroundImg);
-}
-
-update = function() {
-
+    setInterval(update, 50);
 };
 
-setInterval(update, 50);
