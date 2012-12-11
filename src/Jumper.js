8d1634de2f1f8d08321b145dc80d99bfaa1fcad5
Alexander <alsevdokimov@gmail.com>
2012-12-11 16:26:20 +0200

    Try to move some slat

--- src/Jumper.js
+++ src/Jumper.js
@@ -50,7 +50,7 @@ function Jumper(canvas, util) {
     this.i_yPos = canvas.height * 0.7;
     this.i_xAcc = 0;
     this.i_yAcc = 3;
-    this.i_gravity = 2;
+    this.i_gravity = 0.003322259 * canvas.height;
     this.i_maxJumperYPos = canvas.height * 0.5;
     this.i_score = 0;
     this.i_regenerateHeight = 0;
--- src/Slat.js
+++ src/Slat.js
@@ -7,6 +7,22 @@ function Slat(xpos, ypos, slatType) {
 
     this.t = "";
 
+    typeOneMoving = function() {
+        if(this.i_xSlatPos == 'undefined' || this.i_ySlatPos == 'undefined') {
+        this.i_xSlatPos = xpos;
+        this.i_ySlatPos = ypos;
+        }
+        this.i_xSlatPos += 3;
+        //alert(this.i_xSlatPos + " " + xpos + " " + ypos);
+    };
+
+    if(this.slatType == 0) {
+
+        setInterval(typeOneMoving, 50);
+    }
+
+
+
     this.shiftSlat = function(height) {
         this.i_ySlatPos -= height;
     };
